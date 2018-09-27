da9e4e8bc4cd2a76c3012a7763da664ea8e0381a
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-09-27 14:51:07 +0700

    adding username to custom usercreationforms

--- users/forms.py
+++ users/forms.py
@@ -7,11 +7,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('email', 'password1', 'password2',)
+        fields = ('username', 'email', 'password1', 'password2',)
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('email', 'password',)
+        fields = ('username', 'email', 'password',)
