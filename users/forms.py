0eae7964203c7e365b9897bf5b071d0a7d9e10bd
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-10-01 20:40:35 +0700

    fixed register.html and added validation

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,29 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
+from django import forms
+from django.contrib.auth.password_validation import validate_password
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("That username is already taken")
+        return username
+
+    def clean_password1(self):
+        password1 = self.cleaned_data['password1']
+        if validate_password(password1):
+            raise forms.ValidationError('This password is not valid')
+        return password1
+
+    def clean(self, *args, **kwargs):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 != password2:
+            raise forms.ValidationError("Your passwords do not match! Please try again")
+        return super(UserCreationForm, self).clean(*args, **kwargs)
 
     class Meta(UserCreationForm.Meta):
         model = User
