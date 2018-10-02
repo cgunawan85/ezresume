41ccbdffb80fe8858e1992cf39d6689d3028c934
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-10-02 19:35:12 +0700

    added empty state for myresumes.html

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Resume(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+        self.updated_at = timezone.now()
+        return super(Resume, self).save(*args, **kwargs)
