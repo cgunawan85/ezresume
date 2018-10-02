fc64f4cefac6b8cbb168deb954df9b970dca5472
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-10-02 20:11:14 +0700

    added create page with html form

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.my_resumes, name='my-resumes'),
+    path('resume/', views.create_resume, name='create-resume'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def my_resumes(request):
     return render(request, 'app/myresumes.html')
+
+
+def create_resume(request):
+    return render(request, 'app/resume.html')
