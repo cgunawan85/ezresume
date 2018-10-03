31a261292de3571820c7bcbd0ef097a3c9bce463
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-10-03 19:37:50 +0700

    fixed some styling and fixed empty state conditional in myresumes

--- resumes/models.py
+++ resumes/models.py
@@ -3,11 +3,10 @@ from users.models import User
 
 
 class Resume(models.Model):
-    name = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
