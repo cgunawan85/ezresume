98f127aefc6f604a389dc73ad0b76a126cf1b878
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-11-26 21:59:16 +0700

    used google fonts instead of static font files
    new_york.html still buggy when using pdfcrowd

--- resumes/views.py
+++ resumes/views.py
@@ -1,9 +1,9 @@
 import logging
 
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.urls import reverse
